097d31af219b9d86f1d485a652c23513b71d6b77
eugene <jeka.tomchuk@gmail.com>
2022-11-14 16:34:23 +0200

    Completed tasks

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-'use strict' 
+"use strict";
+
+// 1. Створити дві змінні з числами 45 і 12 і виконати з ними всі аріфметичні операції з логуванням.
+
+let num1 = 45;
+let num2 = 12;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+// ----------------------------------------------
+
+// 2. Переробіть код, використовуючи оператори присвоєння, інкремент і декремент.
+
+let number = 456;
+
+number += 6;
+number++;
+number /= 2;
+number -= 3;
+number--;
+number %= 4;
+number += "qwerty";
